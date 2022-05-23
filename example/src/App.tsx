9ef1fa05d43bd0b4960f02a64070ccdf0d61a7e3
Ales Saska <ales@quix.ai>
2022-05-23 21:41:03 +0200

    first commit

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SayHello from 'react-api-king';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <SayHello name="Prateek" />
+    </div>
+  );
+}
+
+export default App;
